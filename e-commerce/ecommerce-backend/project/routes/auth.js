d2c2371f73fd105a0cf510fc58be8a6fd252854c
nallari <shruti.nallari@gmail.com>
2025-07-13 14:57:27 -0400

    pool, bcrypt , adding Register users and login

--- e-commerce/ecommerce-backend/project/routes/auth.js
+++ e-commerce/ecommerce-backend/project/routes/auth.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from "bcrypt";
 import pool from './db.js'
-const router=express.Router()
+const router = express.Router()
 
 
 router.post('/registerUser', async (req, res) => {
@@ -18,5 +18,35 @@ router.post('/registerUser', async (req, res) => {
     }
 })
 
+router.post("/loginUser", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (result.rows.length > 0) {
+            const isMatch = await bcrypt.compare(password, result.rows[0].password);
+            if (isMatch) {
+                res.status(200).json({ message: "Login Successful", accessToken: "token" });
+            } else {
+                res.status(401).json({ message: "Not Authorized" });
+            }
+        } else {
+            res.status(401).json({ message: "Not Authorized" });
+        }
+    } catch (error) {
+        console.error('Error logging in user:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+router.get('/getUsers', async (req, res) => {
+    try {
+        console.log("rq", req)
+        const result = await pool.query('SELECT * FROM users');
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
 
 export default router
